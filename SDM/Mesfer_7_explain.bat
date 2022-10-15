2579bc8168478f6dc906f0863b198f2d7ea1026b
Ajinkya Bharatraj Patil <52972204+Ajinkya-IISERB@users.noreply.github.com>
2022-10-15 11:05:40 +0530

    Delete SDM directory

--- SDM/Mesfer_7_explain.bat
+++
@@ -1,2 +0,0 @@
-java -mx984m -cp "/D:/IISER/MS Thesis/Species distribution modelling/Maxent for Mesfer/maxent.jar;D:\IISER\MS Thesis\Species distribution modelling\Maxent for Mesfer\maxent.jar" density.Explain -l "D:\IISER\MS Thesis\Species distribution modelling\Maxent for Mesfer\results\Mesfer_7.lambdas" -c "D:\IISER\MS Thesis\Species distribution modelling\Maxent for Mesfer\results\Mesfer_7.asc" "D:\IISER\MS Thesis\Species distribution modelling\Maxent for Mesfer\layers"
-@if errorlevel 1 pause
