54ff30c6b81bc54840b686c2e1fb7f1651e77579
Ajinkya Bharatraj Patil <52972204+Ajinkya-IISERB@users.noreply.github.com>
2022-10-15 11:01:46 +0530

    Add files via upload

---
+++ SDM/Mesfer_2_explain.bat
@@ -0,0 +1,2 @@
+java -mx984m -cp "/D:/IISER/MS Thesis/Species distribution modelling/Maxent for Mesfer/maxent.jar;D:\IISER\MS Thesis\Species distribution modelling\Maxent for Mesfer\maxent.jar" density.Explain -l "D:\IISER\MS Thesis\Species distribution modelling\Maxent for Mesfer\results\Mesfer_2.lambdas" -c "D:\IISER\MS Thesis\Species distribution modelling\Maxent for Mesfer\results\Mesfer_2.asc" "D:\IISER\MS Thesis\Species distribution modelling\Maxent for Mesfer\layers"
+@if errorlevel 1 pause
